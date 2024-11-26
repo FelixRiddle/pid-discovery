26ffbc0b18993c96ce3c58d631d0ca49720647e6
Felix Riddle <fedecraft3312@gmail.com>
2024-11-26 17:17:49 -0300

    Added a single function

--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,7 @@
-import fs from "fs";
-import path from "path";
+import { updateAppInfo, upsertProcessInfo, upsertProcessInfoSequelize } from "./processInfo";
 
-/**
- * Update app information on the database
- */
-export async function updateAppInfo() {
-    // Get cwd
-    const CWD = process.cwd();
-    const raw = fs.readFileSync(path.join(CWD, "package.json"));
-    const packageJson = JSON.parse(raw.toString());
-    
-    // Insert / Update process to the database
-    const url = `http://localhost:24000`;
-    const headers = {
-        "Content-Type": "application/json"
-    };
-    const data = {
-        name: packageJson.name,
-        pid: process.pid,
-        appType: "application",
-        url: ``,
-    };
-    
-    return await fetch(`${url}/process`, {
-        method: "POST",
-        headers,
-        body: JSON.stringify(data)
-    });
-}
-
-export interface AppInfo {
-    name: string;
-    pid: number;
-    appType: string;
-    url: string;
-}
-
-/**
- * More configurable option to insert process information
- */
-export async function upsertProcessInfo(appInfo: AppInfo) {
-    // Insert / Update process to the database
-    const url = `http://localhost:24000`;
-    const headers = {
-        "Content-Type": "application/json"
-    };
-    const data = {
-        name: appInfo.name,
-        pid: appInfo.pid,
-        appType: appInfo.appType,
-        url: appInfo.url,
-    };
-    
-    return await fetch(`${url}/process`, {
-        method: "POST",
-        headers,
-        body: JSON.stringify(data)
-    })
-}
+export {
+    updateAppInfo,
+    upsertProcessInfo,
+    upsertProcessInfoSequelize
+};
---
+++ src/processInfo.ts
@@ -0,0 +1,76 @@
+import { Models } from "felixriddle.ts-app-models";
+import fs from "fs";
+import path from "path";
+
+/**
+ * Update app information on the database
+ */
+export async function updateAppInfo() {
+	// Get cwd
+	const CWD = process.cwd();
+	const raw = fs.readFileSync(path.join(CWD, "package.json"));
+	const packageJson = JSON.parse(raw.toString());
+
+	// Insert / Update process to the database
+	const url = `http://localhost:24000`;
+	const headers = {
+		"Content-Type": "application/json",
+	};
+	const data = {
+		name: packageJson.name,
+		pid: process.pid,
+		appType: "application",
+		url: ``,
+	};
+
+	return await fetch(`${url}/process`, {
+		method: "POST",
+		headers,
+		body: JSON.stringify(data),
+	});
+}
+
+export interface AppInfo {
+	name: string;
+	pid: number;
+	appType: string;
+	url: string;
+}
+
+/**
+ * More configurable option to insert process information
+ */
+export async function upsertProcessInfo(appInfo: AppInfo) {
+	// Insert / Update process to the database
+	const url = `http://localhost:24000`;
+	const headers = {
+		"Content-Type": "application/json",
+	};
+	const data = {
+		name: appInfo.name,
+		pid: appInfo.pid,
+		appType: appInfo.appType,
+		url: appInfo.url,
+	};
+
+	return await fetch(`${url}/process`, {
+		method: "POST",
+		headers,
+		body: JSON.stringify(data),
+	});
+}
+
+/**
+ * Insert / Update process information with sequelize
+ */
+export async function upsertProcessInfoSequelize(models: Models, appInfo: AppInfo) {
+	const { Process } = models;
+
+    // Insert / Update model
+    return await Process.upsert({
+        name: appInfo.name,
+        pid: appInfo.pid,
+        appType: appInfo.appType,
+        url: appInfo.url,
+    });
+}
